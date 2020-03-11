8b7bef8df9445b0a869d89f824583826b45d7287
Ngoc Hoang <sounj142@gmail.com>
2020-03-11 07:41:19 +0700

    version 0.6.0 preview

--- src/HLSoft.BlazorWebAssembly.Authentication.OpenIdConnect/CommonExtensions.cs
+++ src/HLSoft.BlazorWebAssembly.Authentication.OpenIdConnect/CommonExtensions.cs
@@ -5,6 +5,9 @@ namespace HLSoft.BlazorWebAssembly.Authentication.OpenIdConnect
 {
     public static class CommonExtensions
     {
+		/// <summary>
+		/// check if the uri is reletive, convert it to absolute otherwise keep it as-is
+		/// </summary>
 		public static string GetAbsoluteUri(this NavigationManager navigationManager, string uri)
 		{
 			if (uri == null) return null;
