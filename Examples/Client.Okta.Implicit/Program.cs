c49191db3a1e42542c542d887a84e129c3d44586
Hoang Luong <hoang.luong@altsrc.net>
2020-03-11 15:14:53 +0700

    Add Okta Example

---
+++ Examples/Client.Okta.Implicit/Program.cs
@@ -0,0 +1,43 @@
+﻿using HLSoft.BlazorWebAssembly.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace Client.Okta.Implicit
+{
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var builder = WebAssemblyHostBuilder.CreateDefault(args);
+			ConfigureServices(builder.Services);
+
+			builder.RootComponents.Add<App>("app");
+
+			await builder.Build().RunAsync();
+		}
+
+		public static void ConfigureServices(IServiceCollection services)
+		{
+			services.AddAuthorizationCore(options => { })
+				.AddBlazoredOpenIdConnect(options =>
+				{
+					options.Authority = "https://dev-902761-admin.okta.com";
+
+					options.ClientId = "0oa3adolwTw5lngzr4x6";
+
+					//options.ResponseType = "token id_token";
+					options.ResponseType = "code";
+
+					//options.WriteErrorToConsole = true;
+					options.RevokeAccessTokenOnSignout = true;
+
+					options.PopupSignInRedirectUri = "/signin-popup-redirect";
+					options.PopupSignOutRedirectUri = "/signout-popup-redirect";
+
+					options.Scope.Add("openid");
+					options.Scope.Add("profile");
+				});
+		}
+	}
+}
